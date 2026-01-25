c5e1604a4eb1a698f4957ab4ea0af603f4d01c8d
Pascal Heraud <heraud@tatm.com>
2026-01-26 00:17:45 +0100

    Added support for UUID + tests containers upgrade to 2.0.3

--- src/test/resources/test-schema-mariadb-init.sql
+++ src/test/resources/test-schema-mariadb-init.sql
@@ -12,6 +12,7 @@ CREATE TABLE users (
     first_name VARCHAR(255),
     last_name VARCHAR(255),
     email VARCHAR(255) UNIQUE NOT NULL,
+    external_id CHAR(36) UNIQUE,
     status ENUM('ACTIVE', 'INACTIVE', 'SUSPENDED'),
     address JSON,
     preferences JSON,
